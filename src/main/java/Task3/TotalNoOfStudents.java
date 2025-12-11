bcf1d6384dbfeac32f6a82eff336132d147a6fc5
Alan K Joseph <alankjoseph29@gmail.com>
2025-12-11 14:49:13 +0530

    Change database connection password
    Updated database connection password for security.

--- src/main/java/Task3/TotalNoOfStudents.java
+++ src/main/java/Task3/TotalNoOfStudents.java
@@ -10,7 +10,7 @@ public class TotalNoOfStudents {
 
 	public static void main(String[] args) {
 		try {
-			Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/student_db", "root", "Root@123");
+			Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/student_db", "root", "Your_password");
 			
 			PreparedStatement ps=con.prepareStatement("select * from student;");
 			
