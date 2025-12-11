f30f172871e28c03f4f40e973c067791fa5526f5
Alan K Joseph <alankjoseph29@gmail.com>
2025-12-11 14:45:29 +0530

    Update database connection password

--- src/main/java/StudentDemo/InsertOneStudentUsingStatement.java
+++ src/main/java/StudentDemo/InsertOneStudentUsingStatement.java
@@ -10,7 +10,7 @@ public class InsertOneStudentUsingStatement {
 
 	public static void main(String[] args) throws SQLException {
 		
-		Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/student_db", "root", "Root@123");
+		Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/student_db", "root", "Your_password");
 
 		Statement stmt=con.createStatement();
 		
